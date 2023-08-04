5c50b1870ec84a095267f66a45c1cfc85fe1d384
lynn-ling <m13898498790_1@163.com>
2023-08-04 12:21:13 +0800

    责任链（前期准备2）

--- src/main/java/com/luckydog/dp/cor/Main.java
+++ src/main/java/com/luckydog/dp/cor/Main.java
@@ -5,21 +5,15 @@ public class Main {
         Msg msg = new Msg();
         msg.setMsg("大家好:)，<script>,欢迎访问 mashibing.com，大家都是996");
 
-        //处理msg
-        String r = msg.getMsg();
-        r = r.replace('<','[');
-        r = r.replace('>',']');
-        msg.setMsg(r);
-
-        r = r.replaceAll("996","955");
-        msg.setMsg(r);
+        new HTMLFilter().doFilter(msg);
+        new SensitiveFilter().doFilter(msg);
 
         System.out.println(msg);
+        //Msg{msg='大家好:)，[script],欢迎访问 mashibing.com，大家都是955'}
     }
 }
 
 class Msg {
-    String name;
     String msg;
 
     public String getMsg() {
@@ -35,5 +29,29 @@ class Msg {
         return "Msg{" +
                 "msg='" + msg + '\'' +
                 '}';
+    }
+}
+
+//定义接口，让其他的filter的相关具体处理来实现这个接口的doFilter方法即可
+interface Filter {
+    void doFilter(Msg m);
+}
+
+class HTMLFilter implements Filter{
+    @Override
+    public void doFilter(Msg m) {
+        String r = m.getMsg();
+        r = r.replace('<','[');
+        r = r.replace('>',']');
+        m.setMsg(r);
+    }
+}
+
+class SensitiveFilter implements Filter{
+    @Override
+    public void doFilter(Msg m) {
+        String r = m.getMsg();
+        r = r.replaceAll("996","955");
+        m.setMsg(r);
     }
 }
