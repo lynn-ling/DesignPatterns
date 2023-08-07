c7432f00115bd9391146c6787ba24944e4a725d8
lynn-ling <m13898498790_1@163.com>
2023-08-07 10:49:48 +0800

    责任链

--- src/main/java/com/luckydog/dp/cor/Main.java
+++ src/main/java/com/luckydog/dp/cor/Main.java
@@ -3,14 +3,24 @@ package com.luckydog.dp.cor;
 import java.util.ArrayList;
 import java.util.List;
 
+//需求：由FilterChain中的某一个Filter决定链条是否继续
+
 public class Main {
     public static void main(String[] args) {
         Msg msg = new Msg();
-        msg.setMsg("大家好:)，<script>,欢迎访问 mashibing.com，大家都是996");
+        //情况①
+//        msg.setMsg("大家好:)，<script>,欢迎访问 mashibing.com，大家都是996");
+        //情况②
+        msg.setMsg("大家好:)，<script>,欢迎访问 mashibing.com，大家都是992");
 
         FilterChain fc = new FilterChain();
         fc.add(new HTMLFilter()).add(new SensitiveFilter());
 
+        /*
+        因为SensitiveFilter在链条上比fc2这个链条靠前
+        情况①：当SensitiveFilter后return false时，fc2链条的过滤都不执行
+        情况②：当SensitiveFilter后return true时，fc2链条的过滤继续执行
+         */
         FilterChain fc2 = new FilterChain();
         fc2.add(new FaceFilter()).add(new URLFilter());
 
@@ -18,7 +28,14 @@ public class Main {
         fc.doFilter(msg);
 
         System.out.println(msg);
-        //Msg{msg='大家好^V^，[script],欢迎访问 http://www.mashibing.com，大家都是955'}
+        /*
+        情况①：
+        Msg{msg='大家好:)，[script],欢迎访问 mashibing.com，大家都是955'}
+        可以看到笑脸没有被替换，同时网址也没有变成过滤器里定义的那样
+        情况②：
+        Msg{msg='大家好^V^，[script],欢迎访问 http://www.mashibing.com，大家都是992'}
+        没有关键字996，因此后面的链条过滤器继续执行
+         */
     }
 }
 
@@ -42,43 +59,54 @@ class Msg {
 }
 
 interface Filter {
-    void doFilter(Msg m);
+    //当doFilter返回是true的时候链条继续往下执行，返回是false的时候就不继续往下执行了
+    boolean doFilter(Msg m);
 }
 
 class HTMLFilter implements Filter{
     @Override
-    public void doFilter(Msg m) {
+    public boolean doFilter(Msg m) {
         String r = m.getMsg();
         r = r.replace('<','[');
         r = r.replace('>',']');
         m.setMsg(r);
+        return true;
     }
 }
 
+//SensitiveFilter这里return的是false，所以如果有996这个敏感词，那么它后面的链条就不会再执行
+//如果没有996这个敏感词，它后面的链条就会执行
 class SensitiveFilter implements Filter{
     @Override
-    public void doFilter(Msg m) {
+    public boolean doFilter(Msg m) {
         String r = m.getMsg();
-        r = r.replaceAll("996","955");
-        m.setMsg(r);
+        if(r.contains("996")){
+            r = r.replaceAll("996","955");
+            m.setMsg(r);
+            return false;
+        }
+        return true;
+
     }
 }
 
 class FaceFilter implements Filter{
     @Override
-    public void doFilter(Msg m) {
+    public boolean doFilter(Msg m) {
         String r = m.getMsg();
         r = r.replace(":)","^V^");
         m.setMsg(r);
+        return true;
     }
 }
 
 class URLFilter implements Filter{
     @Override
-    public void doFilter(Msg m) {
+    public boolean doFilter(Msg m) {
         String r = m.getMsg();
         r = r.replaceAll("mashibing.com","http://www.mashibing.com");
         m.setMsg(r);
+        return true;
     }
 }
 
@@ -90,9 +118,10 @@ class FilterChain implements Filter{
         return this;
     }
 
-    public void doFilter(Msg m) {
+    public boolean doFilter(Msg m) {
         for(Filter f:filters){
-            f.doFilter(m);
+            if(!f.doFilter(m)) return false;
         }
+        return true;
     }
 }
